26ebd8a430f1aa4308de1dd04b502e1369be4e08
Furkat <furkat.khalilov@gmail.com>
2021-02-11 03:23:56 -0500

    partial commit

--- src/test/java/steps/TestCaseAssertions.java
+++ src/test/java/steps/TestCaseAssertions.java
@@ -2,6 +2,7 @@ package steps;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
+import org.testng.Assert;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ public class TestCaseAssertions {
     @Then("User verifies the test case in {string}")
     public void userVerifiesTheTestCaseIn(String page, DataTable dataTable) {
         List<String> allElements = dataTable.asList(String.class);
+
 
 
     }
